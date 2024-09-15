4cd8cd70d8cfbfd36d5bf28907724d8059a69713
Administrador <dahiamarty@gmail.com>
2024-09-15 12:00:09 -0300

    Portafolio con diseño responsive

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.querySelector('.bienvenida');
+
+    div.addEventListener('click', () => {           
+        alert('¡Bienvenid@ a mi portafolio web!');
+    });
+    });
